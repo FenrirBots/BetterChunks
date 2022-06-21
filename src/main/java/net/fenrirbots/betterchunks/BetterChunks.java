37ad4cc8e42b45d5fccb15558a0f4caa9105ec69
FenrirBots <97492893+FenrirBots@users.noreply.github.com>
2022-06-20 22:20:26 -0700

    Base Source Files
    Added all the base source files.

---
+++ src/main/java/net/fenrirbots/betterchunks/BetterChunks.java
@@ -0,0 +1,26 @@
+package me.fenrirbots.betterchunks;
+
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "betterchunks", useMetadata = true, clientSideOnly = true)
+public class FML
+{
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void init(FMLPreInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
